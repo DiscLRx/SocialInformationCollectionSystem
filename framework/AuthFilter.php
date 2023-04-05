57c550878605043b905ac2e01cb760804c5e8371
DiscoIoR <rmfield@qq.com>
2023-04-06 00:26:48 +0800

    规范AuthFilter接口

--- framework/AuthFilter.php
+++ framework/AuthFilter.php
@@ -5,11 +5,11 @@ namespace framework;
 interface AuthFilter {
     /**
      * @param string|NULL $token 身份验证令牌
-     * @param $class_name
+     * @param string $class_name
      * @param string $func_name 要执行的controller方法名称
      * @return bool true for accept, false for denied
      */
-    public function do_filter(?string $token, $class_name, string $func_name) : bool;
+    public function do_filter(?string $token, string $class_name, string $func_name) : bool;
     public function do_after_accept(): void;
     public function do_after_denied(): void;
 }
