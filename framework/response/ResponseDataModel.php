a0a0c703dd499ae5c765d14eb6bdf4ce839b271a
DiscoIoR <rmfield@qq.com>
2023-04-07 14:24:54 +0800

    添加PHPDoc

--- framework/response/ResponseDataModel.php
+++ framework/response/ResponseDataModel.php
@@ -3,8 +3,17 @@
 namespace framework\response;
 
 class ResponseDataModel extends ResponseModel {
+    /**
+     * @var mixed 响应的数据
+     */
     public mixed $data;
 
+
+    /**
+     * @param int $code     状态码
+     * @param mixed $data   响应数据, 此参数为对象时，只有public字段会被序列化为json
+     * @
+     */
     public function __construct(int $code, mixed $data) {
         parent::__construct($code);
         $this->data = $data;
