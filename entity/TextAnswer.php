286e3f3a76456f040c284f1da7d66d4b8155c1c9
DiscoIoR <rmfield@qq.com>
2023-04-08 15:47:26 +0800

    补充填空题作答记录实体类成员

--- entity/TextAnswer.php
+++ entity/TextAnswer.php
@@ -5,10 +5,12 @@ namespace entity;
 class TextAnswer {
     private int $question_id;
     private int $user_id;
+    private string $text;
 
-    public function __construct(int $question_id, int $user_id) {
+    public function __construct(int $question_id, int $user_id, string $text) {
         $this->question_id = $question_id;
         $this->user_id = $user_id;
+        $this->text = $text;
     }
 
     public function get_question_id(): int {
@@ -21,6 +23,14 @@ class TextAnswer {
 
     public function get_user_id(): int {
         return $this->user_id;
+    }
+
+    public function get_text(): string {
+        return $this->text;
+    }
+
+    public function set_text(string $text): void {
+        $this->text = $text;
     }
 
     public function set_user_id(int $user_id): void {
