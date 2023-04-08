7ff08a66bacd55df4c06ddf5f610541c1de7c3cf
cms <304553791@qq.com>
2023-04-08 15:51:15 +0800

    新增部分接口方法声明

--- dao/ChoiceAnswerDao.php
+++ dao/ChoiceAnswerDao.php
@@ -2,6 +2,34 @@
 
 namespace dao;
 
+require_once 'entity/Option.php';
+require_once 'entity/User.php';
+
+/**
+ * 数据库表choice_answer接口
+ */
 interface ChoiceAnswerDao {
+
+    /**
+     * 根据选项id查询用户
+     * @param int $option_id    选项id
+     * @return array            包含User对象的array
+     */
+    public function select_user_by_optionid(int $option_id): array;
+
+    /**
+     * 根据用户id查询选项
+     * @param int $user_id  用户id
+     * @return array        包含Option对象的array
+     */
+    public function select_option_by_userid(int $user_id): array;
+
+    /**
+     * 新增选择题作答记录
+     * @param int $option_id    选项id
+     * @param int $user_id      用户id
+     * @return int              受影响的行数
+     */
+    public function insert(int $option_id, int $user_id): int;
 
 }
--- dao/QuestionDao.php
+++ dao/QuestionDao.php
@@ -2,6 +2,53 @@
 
 namespace dao;
 
-interface QuestionDao {
+use entity\Question;
+
+require_once 'entity/Question.php';
+
+/**
+ *数据库表Question接口
+ */
+interface  QuestionDao{
+    /**
+     * 查询所有问题
+     * @return array    包含Question对象的array
+     */
+    public function select(): array;
+
+    /**
+     * 根据问题id查询问题
+     * @param int $id       问题id
+     * @return Question     Question对象
+     */
+    public function select_by_id(int $id): Question;
+
+    /**
+     * 根据问卷id查询问题
+     * @param int $questionnaire_id 问卷id
+     * @return array                包含Question对象的array
+     */
+    public function select_by_questionnaireid(int $questionnaire_id): array;
+
+    /**
+     * 新增问题
+     * @param int $questionnaire_id 问卷id
+     * @param int $order            问题序号
+     * @param string $type          问题类型
+     * @param string $content       问题内容
+     * @return int                  受影响的行数
+     */
+    public function insert(int $questionnaire_id, int $order, string $type, string $content): int;
+
+    /**
+     * 更新问题
+     * @param int $id               问题id
+     * @param int $questionnaire_id 问卷id
+     * @param int $order            问题序号
+     * @param string $type          问题类型
+     * @param string $content       问题内容
+     * @return int                  受影响的行数
+     */
+    public function update_by_id(int $id, int $questionnaire_id, int $order, string $type, string $content): int;
 
 }
